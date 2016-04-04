7316e1c6c9d71253a113dce91e5cc1e92b3c0546
Guruprasad Gaonkar <ggaonk@codeaurora.org>
2016-04-04 12:46:43 -0700

    QCamera2: Provide camera output colorspace to video.
    Set the camera output colorspace in native handle
    instead of indicating video encoder output colorspace.
    709 was configured to video to get 709 colorspace output from
    video. But now video to decide final configuration and camera
    to configure input format which is 601.
    
    CRs-Fixed: 997590
    
    Change-Id: Iedf04bc2968ded10c3e536ec6b23dfe246fa4d00

--- QCamera2/HAL/QCameraMem.cpp
+++ QCamera2/HAL/QCameraMem.cpp
@@ -1105,7 +1105,9 @@ QCameraVideoMemory::QCameraVideoMemory(camera_request_memory getMemory,
 {
     memset(mMetadata, 0, sizeof(mMetadata));
     memset(mNativeHandle, 0, sizeof(mNativeHandle));
-     mMetaBufCount = 0;
+    mMetaBufCount = 0;
+    //Set Default color conversion format
+    mUsage |= private_handle_t::PRIV_FLAGS_ITU_R_601_FR;
 }
 
 /*===========================================================================
@@ -1185,7 +1187,8 @@ int QCameraVideoMemory::allocate(int count, int size)
         nh->data[0] = mMemInfo[i].fd;
         nh->data[1] = 0;
         nh->data[2] = mMemInfo[i].size;
-        nh->data[3] = 0;//dummy value for usage
+        //value for usage
+        nh->data[3] = mUsage | private_handle_t::PRIV_FLAGS_ITU_R_601_FR;
         nh->data[4] = 0; //dummy value for timestamp in non-batch mode
         nh->data[5] = mFormat;
         nh->data[6] = i;//buffer index
@@ -1247,7 +1250,8 @@ int QCameraVideoMemory::allocateMore(int count, int size)
         nh->data[0] = mMemInfo[i].fd;
         nh->data[1] = 0;
         nh->data[2] = mMemInfo[i].size;
-        nh->data[3] = 0;//dummy value for usage
+        //value for usage
+        nh->data[3] = mUsage | private_handle_t::PRIV_FLAGS_ITU_R_601_FR;
         nh->data[4] = 0; //dummy value for timestamp in non-batch mode
         nh->data[5] = mFormat;
         nh->data[6] = i;//buffer index

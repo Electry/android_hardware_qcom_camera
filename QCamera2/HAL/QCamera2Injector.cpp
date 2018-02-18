6ec2924c7228e80f2530a7db053467b11635ff08
Michal Chvila <michal@chvila.sk>
2018-02-18 18:36:03 +0100

    QCamera2: Add 4k DCI for back camera
    Change-Id: I785ca6b4bb02a5d79af191e745d89ccfb8691c95
    Signed-off-by: Michal Chvila <michal@chvila.sk>

--- QCamera2/HAL/QCamera2Injector.cpp
+++ QCamera2/HAL/QCamera2Injector.cpp
@@ -6,6 +6,8 @@ namespace qcamera {
 
 void QCamera2Injector::inject(cam_capability_t *camCapability, int cameraId)
 {
+    int i;
+
     ALOGE("%s: Injecting camera parameters for cameraId=%d",
             __func__, cameraId);
 
@@ -13,6 +15,19 @@ void QCamera2Injector::inject(cam_capability_t *camCapability, int cameraId)
         /* supported HFR */
         camCapability->hfr_tbl_cnt = 1;
         camCapability->hfr_tbl[0].mode = CAM_HFR_MODE_60FPS;
+
+        /* supported preview sizes */
+        for (i = camCapability->preview_sizes_tbl_cnt; i > 0; i--)
+            camCapability->preview_sizes_tbl[i] = camCapability->preview_sizes_tbl[i - 1];
+        camCapability->preview_sizes_tbl[0] = {4096, 2160};
+        camCapability->preview_sizes_tbl_cnt++;
+
+        /* supported video sizes */
+        for (i = camCapability->video_sizes_tbl_cnt; i > 0; i--)
+            camCapability->video_sizes_tbl[i] = camCapability->video_sizes_tbl[i - 1];
+        camCapability->video_sizes_tbl[0] = {4096, 2160};
+        camCapability->video_sizes_tbl_cnt++;
+
     }
 
     /* supported fps ranges */

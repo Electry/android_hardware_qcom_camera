ef70d9e8974872173206a3edf16dbbef7367627e
Michal Chvila <michal@chvila.sk>
2018-02-18 17:34:40 +0100

    QCamera2: Replace 120fps with 60fps HFR/HSR
    * 120fps is for some reason broken
       HSR recordings result in 2x sped up video
       HFR recordings are only 2x longer, instead of 4x
    
    * 60fps works fine, use this until we figure out wth goes wrong
    
    Change-Id: I8a4e1c3151b8be5e81b3315ea93c9bf5c5b0a127
    Signed-off-by: Michal Chvila <michal@chvila.sk>

--- QCamera2/HAL/QCamera2Injector.cpp
+++ QCamera2/HAL/QCamera2Injector.cpp
@@ -8,6 +8,12 @@ void QCamera2Injector::inject(cam_capability_t *camCapability, int cameraId)
 {
     ALOGE("%s: Injecting camera parameters for cameraId=%d",
             __func__, cameraId);
+
+    if (cameraId == CAM_POSITION_BACK) {
+        /* supported HFR */
+        camCapability->hfr_tbl_cnt = 1;
+        camCapability->hfr_tbl[0].mode = CAM_HFR_MODE_60FPS;
+    }
 }
 
 }; // namespace qcamera
